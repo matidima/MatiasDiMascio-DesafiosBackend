f376a387759eaf47be4d7509afa275eccbc37f7d
Matias Di Mascio <matias.dimascio@gmail.com>
2022-11-14 20:30:59 -0300

    primera entrega proyecto final

--- Primera entrega/src/routers/routerCarrito.js
+++ Primera entrega/src/routers/routerCarrito.js
@@ -1,79 +1,77 @@
 import fs from "fs";
-import { Router } from 'express'
-import { carritoApi } from "../api/api.js";
+import { Router } from "express";
+import { carritoApi, productApi } from "../api/api.js";
 import { admin } from "../admin/admin.js";
 
-const filepathProductos = 'productos.json'
-const filepathCart = 'carrito.json'
+const filepathProductos = "productos.json";
+const filepathCart = "carrito.json";
 const routerCarrito = Router();
 const carrito = {
-    timestamp: new Date(),
-    productos: []
-}
+  timestamp: new Date(),
+  productos: [],
+};
 
-routerCarrito.get('/', async (req,res) => {
-    const carrito = await carritoApi.getAll(filepathCart)
-    res.send(carrito)
-})
+routerCarrito.get("/", async (req, res) => {
+  const carrito = await carritoApi.getAll(filepathCart);
+  res.send(carrito);
+});
 
-routerCarrito.post('/', async (req, res) => {
-    await carritoApi.save(carrito,filepathCart)
-    const elementos = await carritoApi.getAll(filepathCart)
-    res.send(elementos)
-})
+routerCarrito.post("/", async (req, res) => {
+  await carritoApi.save(carrito, filepathCart);
+  const elementos = await carritoApi.getAll(filepathCart);
+  res.send(elementos);
+});
 
-routerCarrito.delete('/:id', async (req, res) => {
-    const id = req.params.id
-    await carritoApi.deleteById(id,filepathCart)
-    const carritos = await carritoApi.getAll(filepathCart)
-    res.send(carritos)
-})
+routerCarrito.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  await carritoApi.deleteById(id, filepathCart);
+  const carritos = await carritoApi.getAll(filepathCart);
+  res.send(carritos);
+});
 
-routerCarrito.get('/:id/productos', async (req, res) => {
-    const id = req.params.id
-    const objeto = await carritoApi.getById(id,filepathCart)
-    console.log(typeof(objeto.elemento.productos))
-    res.send(objeto.elemento.productos)
-})
+routerCarrito.get("/:id/productos", async (req, res) => {
+  const { id } = req.params;
+  const cart = await carritoApi.getById(id);
+  res.send(cart.productos);
+});
 
-routerCarrito.post('/:id/productos', async (req, res) => {
-    const id = req.params.id
-    let producto = await carritoApi.getById(id,filepathProductos)
-    producto = producto.elemento
-    if(producto === null){
-        res.send('Error: No pudo encontrarse el producto')
-      }else{
-        let cart = await getById(1,filepathCart)
-        cart = cart.elemento
-        const productos = cart.productos
-        productos.push(producto)
-        cart.productos = productos
+routerCarrito.post("/:id/productos", async (req, res) => {
+  const { productId } = req.body;
+  const { id } = req.params;
+  const cart = await carritoApi.getById(Number(id));
+  if (!cart)
+    return res.send({ error: true, message: "Carrito no encontrado" });
 
-        await carritoApi.deleteById(1,filepathCart)
-        await carritoApi.save(cart,filepathCart)
+  const product = await productApi.getById(Number(productId));
 
-        res.send(productos)
+  if (!product)
+    return res.send({ error: true, message: "Producto no encontrado" });
+
+  cart.productos.push(product);
+  const updatedCart = await carritoApi.updateById(Number(id), cart);
+  res.send({ cart: updatedCart });
+});
+
+routerCarrito.delete("/:id/productos/:id_prod", async (req, res) => {
+  const { id } = req.params;
+  const { productId } = req.params;
+  let cart = await carritoApi.getById(id, filepathCart);
+  if (cart === null) {
+    res.send("Error: no se pudo encontrar el carrito");
+  } else {
+    cart = cart.elemento;
+    const productosFiltrados = cart.productos.filter(
+      (elemento) => elemento.id != productId
+    );
+    if (productosFiltrados === null) {
+      res.send("Error: no se pudo encontrar el producto a eliminar");
+    } else {
+      cart.productos = productosFiltrados;
+      await carritoApi.deleteById(id, filepathCart);
+      await carritoApi.save(cart, filepathCart);
+      res.send(cart);
     }
-})
-
-routerCarrito.delete('/:id_cart/productos/:id_prod', async (req,res) => {
-    const {id_cart} = req.params
-    const {id_prod} = req.params
-    let cart = await carritoApi.getById(id_cart,filepathCart)
-    if(cart === null){
-        res.send('Error: no se pudo encontrar el carrito')
-    }else{
-        cart = cart.elemento
-        const productosFiltrados = cart.productos.filter((elemento) => elemento.id != id_prod)
-        if(productosFiltrados === null){
-            res.send('Error: no se pudo encontrar el producto a eliminar')
-        }else{
-            cart.productos = productosFiltrados
-            await carritoApi.deleteById(id_cart,filepathCart)
-            await carritoApi.save(cart,filepathCart)
-            res.send(cart)
-        }
-    }
-})
+  }
+});
 
 export { routerCarrito };
