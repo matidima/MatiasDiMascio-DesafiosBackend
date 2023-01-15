af0c9998bdb8318ef5c0b0f1f4eecdf6a62a3114
mati <matias.dimascio@hotmail.com>
2023-01-15 12:01:39 -0300

    desafio10/log-in por formulario

---
+++ Desafio 10/src/config/index.js
@@ -0,0 +1,12 @@
+const config = {
+    DATABASE: {
+        mongo: {
+            dburl: `mongodb+srv://matidima:HNIlXZrVnoqCt8Rx@cluster0.7ujig2q.mongodb.net/?retryWrites=true&w=majority`,
+            dbName: `backend`
+        }
+    }
+}
+
+export { config }
+
+/* HNIlXZrVnoqCt8Rx   */
